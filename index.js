837ddbd362cd3fe92213f4c36aad517345b20a9c
isabelamihai <isabelaelenamihai@gmail.com>
2025-04-04 12:43:55 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let homeScore =18;
 let guestScore = 10;
 let homeEl = document.getElementById("home-score");
 let guestEl = document.getElementById("guest-score");
+let winnerEL = document.getElementById("winner-message");
 
 // Home functions
 function add1Point(){
@@ -34,4 +35,25 @@ function add2PointsG(){
 function add3PointsG(){
     guestScore+=3;
     guestEl.textContent = guestScore;
+}
+
+function resetScore(){
+    homeScore = 0;
+    guestScore = 0;
+    homeEl.textContent = homeScore;
+    guestEl.textContent = guestScore;
+    winnerEL.textContent = ""; // Clear winner message
+    winnerEL.style.border = "none"; // Reset border
+}
+
+function declareWinner() {
+    if (homeScore > guestScore) {
+       winnerEL.textContent ="Home team wins!";
+    } else if (guestScore > homeScore) {
+        winnerEL.textContent= "Guest team wins!";
+    } else {
+        winnerEL.textContent= "It's a tie!";
+    }   
+    winnerEL.style.border = "3px double #9AABD8";
+    // Reset scores after declaring winner
 }
